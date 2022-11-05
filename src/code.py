7feee24cd04da3974a20a393b8857c6d8bf8f8a2
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-11-05 16:49:56 +1300

    Initial error scenario checking

--- src/code.py
+++ src/code.py
@@ -70,8 +70,7 @@ spi = busio.SPI(board.SCK, board.MOSI)  # Uses SCK and MOSI
 epd_cs = board.D9
 epd_dc = board.D10
 
-i2c = board.I2C()
-scd4x = adafruit_scd4x.SCD4X(i2c)
+
 
 display_bus = displayio.FourWire(
     spi, command=epd_dc, chip_select=epd_cs, baudrate=1000000
@@ -98,28 +97,39 @@ palette[0] = BACKGROUND_COLOR
 t = displayio.TileGrid(background_bitmap, pixel_shader=palette)
 tile_grid.append(t)
 
-co2, temperature, relative_humidity = trigger_single_sensor_measurement(scd4x)
 
-co2_background_rect = Rect(2, 2, 189, 124, fill=DARKGREY, outline=0x0, stroke=0)
-tile_grid.append(co2_background_rect)
 
-temperature_background_rect = Rect(193, 2, 101, 61, fill=DARKGREY, outline=0x0, stroke=0)
-tile_grid.append(temperature_background_rect)
+try:
+    i2c = board.I2C()
+    scd4x = adafruit_scd4x.SCD4X(i2c)
+    
+    co2, temperature, relative_humidity = trigger_single_sensor_measurement(scd4x)
 
-humidity_background_rect = Rect(193, 65, 101, 61, fill=DARKGREY, outline=0x0, stroke=0)
-tile_grid.append(humidity_background_rect)
+    co2_background_rect = Rect(2, 2, 189, 124, fill=DARKGREY, outline=0x0, stroke=0)
+    tile_grid.append(co2_background_rect)
 
-co2_value_text_group = create_text_group(27, 90, terminalio.FONT, "%d ppm" % co2, 3, WHITE)
-tile_grid.append(co2_value_text_group)
+    temperature_background_rect = Rect(193, 2, 101, 61, fill=DARKGREY, outline=0x0, stroke=0)
+    tile_grid.append(temperature_background_rect)
 
-temperature_text_group = create_text_group(199, 30, terminalio.FONT, "%0.1fC" % temperature, 3, WHITE)
-tile_grid.append(temperature_text_group)
+    humidity_background_rect = Rect(193, 65, 101, 61, fill=DARKGREY, outline=0x0, stroke=0)
+    tile_grid.append(humidity_background_rect)
 
-humidity_text_group = create_text_group(199, 90, terminalio.FONT, "%0.1f%%" % relative_humidity, 3, WHITE)
-tile_grid.append(humidity_text_group)
+    co2_value_text_group = create_text_group(27, 90, terminalio.FONT, "%d ppm" % co2, 3, WHITE)
+    tile_grid.append(co2_value_text_group)
 
-co2_text_group = create_text_group(25, 35, verdana_bold_font, get_co2_wording(co2), 2, WHITE)
-tile_grid.append(co2_text_group)
+    temperature_text_group = create_text_group(199, 30, terminalio.FONT, "%0.1fC" % temperature, 3, WHITE)
+    tile_grid.append(temperature_text_group)
+
+    humidity_text_group = create_text_group(199, 90, terminalio.FONT, "%0.1f%%" % relative_humidity, 3, WHITE)
+    tile_grid.append(humidity_text_group)
+
+    co2_text_group = create_text_group(25, 35, verdana_bold_font, get_co2_wording(co2), 2, WHITE)
+    tile_grid.append(co2_text_group)
+
+except RuntimeError as err:
+    error_text_group = create_text_group(10, 10, terminalio.FONT, str(err), 1, BLACK)
+    tile_grid.append(error_text_group)
+
 
 display.show(tile_grid)
 display.refresh()	
